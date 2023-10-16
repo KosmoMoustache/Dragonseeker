1e2ed0e5b3cadae4f0a14aa4dc7df7a23f998331
KosmoMoustache <readyfoxysh@gmail.com>
2023-10-16 17:44:02 +0200

    Add music disc and change register logic

--- src/main/java/syric/dragonseeker/Dragonseeker.java
+++ src/main/java/syric/dragonseeker/Dragonseeker.java
@@ -2,23 +2,28 @@ package syric.dragonseeker;
 
 
 import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.config.ModConfig;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import syric.dragonseeker.registry.DSItems;
+import syric.dragonseeker.registry.DSSounds;
 
-// The value here should match an entry in the META-INF/mods.toml file
-@Mod(Dragonseeker.MODID)
+@Mod(Dragonseeker.MOD_ID)
 public class Dragonseeker {
     public static final Logger LOGGER = LogManager.getLogger();
-    public static final String MODID = "dragonseeker";
-
+    public static final String MOD_ID = "dragonseeker";
 
     public Dragonseeker() {
+        IEventBus eventBus = FMLJavaModLoadingContext.get().getModEventBus();
         ModLoadingContext.get().registerConfig(ModConfig.Type.COMMON, DragonSeekerConfig.COMMON_SPEC);
+
+        DSItems.register(eventBus);
+        DSSounds.register(eventBus);
+
         MinecraftForge.EVENT_BUS.register(this);
-        DSItems.init();
     }
 }
--- src/main/java/syric/dragonseeker/registry/DSItems.java
+++ src/main/java/syric/dragonseeker/registry/DSItems.java
@@ -1,9 +1,7 @@
 package syric.dragonseeker.registry;
 
-import net.minecraft.world.item.Item;
-import net.minecraft.world.item.Items;
-import net.minecraft.world.item.Rarity;
-import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import net.minecraft.world.item.*;
+import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
 import net.minecraftforge.registries.RegistryObject;
@@ -11,14 +9,15 @@ import syric.dragonseeker.Dragonseeker;
 import syric.dragonseeker.item.tool.*;
 
 public class DSItems {
-    public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, Dragonseeker.MODID);
+    public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, Dragonseeker.MOD_ID);
 
-    public static void init() {
-        ITEMS.register(FMLJavaModLoadingContext.get().getModEventBus());
+    public static void register(IEventBus eventBus) {
+        ITEMS.register(eventBus);
     }
 
     public static final RegistryObject<Item> DRAGONSEEKER = ITEMS.register("dragonseeker", () -> new DragonSeekerItem(DragonSeekerItemConfig.get(DragonSeekerItem.SeekerType.Common), Items.NETHERITE_INGOT, 128, Rarity.COMMON));
     public static final RegistryObject<Item> EPIC_DRAGONSEEKER = ITEMS.register("epic_dragonseeker", () -> new DragonSeekerItem(DragonSeekerItemConfig.get(DragonSeekerItem.SeekerType.Epic), Items.NETHERITE_INGOT, 256, Rarity.RARE));
     public static final RegistryObject<Item> LEGENDARY_DRAGONSEEKER = ITEMS.register("legendary_dragonseeker", () -> new LegendaryDragonSeekerItem(DragonSeekerItemConfig.get(DragonSeekerItem.SeekerType.Legendary), Items.NETHERITE_INGOT, 512, Rarity.RARE));
     public static final RegistryObject<Item> GODLY_DRAGONSEEKER = ITEMS.register("godly_dragonseeker", () -> new DragonSeekerItem(DragonSeekerItemConfig.get(DragonSeekerItem.SeekerType.Godly), Items.NETHERITE_INGOT, -1, Rarity.EPIC));
+    public static final RegistryObject<Item> MUSIC_DISC_01 = ITEMS.register("music_disc_01", () -> new RecordItem(4, DSSounds.MUSIC_DISC_01, new Item.Properties().tab(CreativeModeTab.TAB_MISC).stacksTo(1)));
 }
